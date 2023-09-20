dfbc5fccb57beedc260fcc8edbc53585376a391f
John Grimes <john@grimes.id.au>
2023-09-20 04:53:34 +0000

    Update scope to allow read and write of Patient resource

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const smartLaunch = () => {
   FHIR.oauth2
     .init({
       clientId: '52a346b2-7512-4084-934b-98eb44f7d6e7',
-      scope: 'launch/patient openid profile'
+      scope: 'launch/patient openid profile patient/*.cruds'
     })
     .then(client => {
       ReactDOM.render(<App client={client} />, rootElement);
